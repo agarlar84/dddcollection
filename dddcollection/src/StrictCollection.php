962c95ff35fb85cec2307d60d56f93b4b45e4f56
Alex Garcia <alexgarlar@gmail.com>
2016-10-01 09:51:26 +0200

    Added strictCollection

--- dddcollection/src/StrictCollection.php
+++ dddcollection/src/StrictCollection.php
@@ -2,20 +2,10 @@
 
 namespace DDDCollection;
 
-class StrictCollection
+class StrictCollection extends Collection
 {
     public static $collectibleObjectsClassName = '';
     protected $collection = [];
-
-    public function __construct($objClassName, ...$variadicElementsToCollect)
-    {
-        static::$collectibleObjectsClassName = $objClassName;
-
-        $this->collection = [];
-        foreach ($variadicElementsToCollect as $occupancy) {
-            $this->add($occupancy);
-        }
-    }
 
     public function add($anElementToCollect)
     {
@@ -28,46 +18,10 @@ class StrictCollection
         return $this;
     }
 
-    public function getIterator()
-    {
-        return new ArrayIterator($this->collection);
-    }
-
-    public function count()
-    {
-        $count = 0;
-        /** @var Countable $collectedElement */
-        foreach ($this->collection as $collectedElement) {
-            $count += count($collectedElement);
-        }
-
-        return $count;
-    }
-
-    public function offsetExists($offset)
-    {
-        return isset($this->collection[$offset]);
-    }
-
-    public function offsetGet($offset)
-    {
-        return $this->collection[$offset];
-    }
-
     public function offsetSet($offset, $value)
     {
         if (is_a($value, static::$collectibleObjectsClassName)) {
             $this->collection[$offset] = $value;
         }
-    }
-
-    public function offsetUnset($offset)
-    {
-        unset($this->collection[$offset]);
-    }
-
-    public function getCollection()
-    {
-        return $this->collection;
     }
 }
